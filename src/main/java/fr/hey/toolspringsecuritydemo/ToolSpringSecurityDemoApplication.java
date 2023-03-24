bee7b74a04d341731e792e98cef29fbb0b72474d
pierre-hey <83176715+pierre-hey@users.noreply.github.com>
2023-03-24 09:45:58 +0100

    init

---
+++ src/main/java/fr/hey/toolspringsecuritydemo/ToolSpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.hey.toolspringsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolSpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolSpringSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/hey/toolspringsecuritydemo/ToolSpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hey.toolspringsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolSpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
